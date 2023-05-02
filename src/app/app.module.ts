434cf43bd2894573fad0b5b8627cdf66dc42c049
tasnimbenslama <tasnim.benslama@esprit.tn>
2023-05-02 14:24:12 +0100

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,25 @@ import { FooterUserComponent } from './frontOffice/footer-user/footer-user.compo
 import { AllTemplateUserComponent } from './frontOffice/all-template-user/all-template-user.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
 import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
+import {FormsModule ,ReactiveFormsModule} from "@angular/forms";
+import { AddEventComponent } from './Event/add-event/add-event.component';
+import { EventListComponent } from './Event/event-list/event-list.component';
+import { SearchDeleteComponent } from './Event/search-delete/search-delete.component';
+import { ImageDataComponent } from './image-data/image-data.component';
+import { AddTeamComponent } from './TEAM/add-team/add-team.component';
+import { AddProjectComponent } from './PROJECT/add-project/add-project.component';
+import { ListComponent } from './PATERNSHIP/list/list.component';
+import { ListBackComponent } from './PATERNSHIP/list-back/list-back.component';
+import { AddPComponent } from './PATERNSHIP/add-p/add-p.component';
+import { AddUserComponent } from './User/add-user/add-user.component';
+import { ListUserComponent } from './User/list-user/list-user.component';
+import { ReadComponent } from './TEAM/read/read.component';
+import { AddthComponent } from './THEMATIQUE/addth/addth.component';
+import { UpdateComponent } from './Event/update/update.component';
+import { AddDComponent } from './Document/add-d/add-d.component';
+import { AddFComponent } from './Followed/add-f/add-f.component';
+import { AffComponent } from './Followed/aff/aff.component';
+
 
 @NgModule({
   declarations: [
@@ -29,14 +47,35 @@ import {FormsModule} from "@angular/forms";
     FooterUserComponent,
     AllTemplateUserComponent,
     DashbordComponent,
+    AddEventComponent,
+    EventListComponent,
+    SearchDeleteComponent,
+    ImageDataComponent,
+    AddTeamComponent,
+    AddProjectComponent,
+    ListComponent,
+    ListBackComponent,
+    AddPComponent,
+    AddUserComponent,
+    ListUserComponent,
+    ReadComponent,
+    AddthComponent,
+    UpdateComponent,
+    AddDComponent,
+    AddFComponent,
+    AffComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
